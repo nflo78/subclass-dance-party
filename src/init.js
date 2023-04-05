7c05740f986b416d8f0282b762e843e9e4916bc8
Andrew Lund <andrew.s.lund@gmail.com>
2023-04-04 17:03:56 -0700

    Added mouseover and some additional animation

--- src/init.js
+++ src/init.js
@@ -29,20 +29,20 @@ $(document).ready(function() {
     );
     $('body').append(dancer.$node);
     window.dancers.push(dancer.$node);
+
+    $('.dancer').on('mouseover', function(event) {
+      $(this).css({"filter":"hue-rotate(180deg)"});
+    });
   });
 
   $('.lineUpButton').on('click', function(event) {
-    // for each element in window.dancers
     var topVal = 100;
     for (var i = 0; i < window.dancers.length; i++) {
-      // set left property to some constant
-      // console.log(window.dancers[i]);
-      window.dancers[i].animate({
-        "top": topVal.toString(),
-        "left": "30"
-      });
-      // window.dancers[i].setPosition(30, topVal);
-      // set top property to some incremented value
+      window.dancers[i]
+        .animate({
+          "top": topVal.toString(),
+          "left": "30"
+        });
       topVal += 50;
     }
   });
--- src/squareDancer.js
+++ src/squareDancer.js
@@ -8,27 +8,27 @@ makeSquareDancer.prototype.constructor = makeSquareDancer;
 
 makeSquareDancer.prototype.step = function() {
   makeDancer.prototype.step.call(this);
-  var fadeTime = Math.random * this.timeBetweenSteps;
-  // this.$node.fadeToggle(fadeTime);
-  this.$node.animate({
+  this.$node
+    .animate({
     // "left": "+=50px",
     // "top": "+=50px",
-    "rotate": "+=720deg",
-    "height": "+=5px",
-    "width": "+=5px"
-  }, "slow");
-  this.$node.animate({
-    "left": "+=50px"
-  }, "slow");
-  this.$node.animate({
-    "top": "+=50px"
-  }, "slow");
-  this.$node.animate({
-    "left": `-=50px`
-    // "top": "+=50px",
-  }, "slow");
-  this.$node.animate({
-    // "left": "+=50px",
-    "top": `-=50px`
-  }, "slow");
+      "rotate": "+=720deg",
+      "height": "+=5px",
+      "width": "+=5px"
+    }, "slow")
+    .animate({
+      "left": "+=50px"
+    }, "slow")
+    .animate({
+      "top": "+=50px"
+    }, "slow")
+    .animate({
+      "left": `-=50px`
+      // "top": "+=50px",
+    }, "slow")
+    .animate({
+      // "left": "+=50px",
+      "top": `-=50px`
+    }, "slow");
+
 };
